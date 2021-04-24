9a92513314bd1bf3d5e4c58d0b5b7f0f78a03687
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-24 16:09:08 +0200

    adds 404 error handling for invalid Article ID`s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :no_such_record
+
+  private
+
+  def no_such_record
+    render json: {status: 404, message: "No such article"}
+  end
 end
