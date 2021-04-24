7a4423394e786eccd86bd90fddab5ef849a03042
DavDan1 <david-danielyan@hotmail.com>
2021-04-24 17:48:15 +0200

    setup for tocken authintication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+        include DeviseTokenAuth::Concerns::SetUserByToken
   rescue_from ActiveRecord::RecordNotFound, with: :no_such_record
 
   private
