2ae65e2f157c752baa7f90858fb7c4c4456a9b37
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-24 15:25:07 +0200

    minor refactoring

--- spec/requests/api/db:api_responds_with_a_collection_of_articles_spec.rb
+++ spec/requests/api/db:api_responds_with_a_collection_of_articles_spec.rb
@@ -1,22 +1,22 @@
-RSpec.describe 'GET /api/articles' do
-  describe 'succesfully' do
-    let!(:article_1) { create(:article, title: 'This is an awesome title') }
+RSpec.describe "GET /api/articles" do
+  describe "succesfully" do
+    let!(:article_1) { create(:article, title: "This is an awesome title") }
     let!(:articles_2_3) { 2.times { create(:article) } }
     before do
-      get '/api/articles'
+      get "/api/articles"
     end
 
-    it 'is expected to return 200 status' do
+    it "is expected to return 200 status" do
       expect(response).to have_http_status 200
     end
-
-    it 'is expected to return all articles' do
+    
+    it "is expected to return all articles" do
       binding.pry
-      expect(response_json['articles'].count).to eq 3
+      expect(response_json["articles"].count).to eq 3
     end
 
-    it 'is expected to return article titles' do
-      expect(response_json['articles'].first['title']).to eq 'This is an awesome title'
+    it "is expected to return article titles" do
+      expect(response_json["articles"].first["title"]).to eq "This is an awesome title"
     end
   end
 end
