edca6879d85184fd0d0410d5e7b9b2c142ffd26e
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-25 12:41:48 +0200

    excepts index and show action from user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-  before_action :authenticate_user!
+  
+  before_action :authenticate_user!, except: [:index, :show]
   
   rescue_from ActiveRecord::RecordNotFound, with: :no_such_record
 
